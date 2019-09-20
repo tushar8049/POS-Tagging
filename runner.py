2860a90062cb40883e01035570941ce40fcc7aad
tushar-gonawala <tushar.gonawala@copart.com>
2019-09-20 18:09:24 -0500

    Load_Corpus Implemented!
    Returning The List of List of size 57340

--- runner.py
+++ runner.py
@@ -1,6 +1,9 @@
 import tagger as tag
 
+
+corpus_path = 'brown_corpus_modified'
+
 myTagger = tag.Tagger()
-tokenList = myTagger.load_corpus('brown_corpus_modified')
+tokenList = myTagger.load_corpus(corpus_path)
 
 print(tokenList)
