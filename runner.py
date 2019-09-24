cc861251f4e14da7d26824eaa56ea5b9df5777f3
tushar-gonawala <tushar.gonawala@copart.com>
2019-09-24 16:53:25 -0500

    Implemented Viterbi Decode and Tested on two sentences. Seems good!

--- runner.py
+++ runner.py
@@ -1,24 +1,34 @@
 import tagger as tag
 
-corpus_path = 'brown_corpus_modified'
 
+"""
+Loading Corpus
+"""
+corpus_path = 'brown_corpus_modified'
 myTagger = tag.Tagger()
 tokenList = myTagger.load_corpus(corpus_path)
 
-# print(tokenList)
 
+"""
+Initializing Frequencies and then the Probabilities for:
+Initial Tagging, Transition, Emission
+"""
 for sentence in tokenList:
     myTagger.initialize_frequencies(sentence)
-
 myTagger.initialize_probabilities()
-print(myTagger.get_initial_tag_frequency())
-print(myTagger.get_transition_total())
+# print("Initial Tag Probability: \n", myTagger.get_initial_tag_probability())
+#
+# print("Transition Probability: \n", myTagger.get_transition_probability())
+#
+# print("Emission Probability: \n", myTagger.get_emission_probability())
 
-print(myTagger.get_emission_total())
-print(myTagger.get_word_total())
-print("Initial Tag Probability: \n", myTagger.get_initial_tag_probability())
 
-print("Transition Probability: \n", myTagger.get_transition_probability())
-
-print("Emission Probability: \n", myTagger.get_emission_probability())
-
+"""
+Testing the Tagging using Viterbi Decode for two sentences:
+1. The Secretariat is expected to race tomorrow .
+2. People continue to enquire the reason for the race for outer space .
+"""
+sentence1 = "The Secretariat is expected to race tomorrow ."
+sentence2 = "People continue to enquire the reason for the race for outer space ."
+print("Tagging of Sentence 1: ", myTagger.viterbi_decode(sentence1))
+print("Tagging of Sentence 2: ", myTagger.viterbi_decode(sentence2))
--- tagger.py
+++ tagger.py
@@ -60,8 +60,9 @@ class Tagger:
                     word_token[1] in self.token_frequency.keys()) else 1
 
         for i in range(0, len(sentence) - 1):
-            self.transition_frequency[(sentence[i][1], sentence[i+1][1])] = (self.transition_frequency.pop((sentence[i][1], sentence[i+1][1])) + 1) if (
-                    (sentence[i][1], sentence[i+1][1]) in self.transition_frequency.keys()) else 1
+            self.transition_frequency[(sentence[i][1], sentence[i + 1][1])] = (
+                    self.transition_frequency.pop((sentence[i][1], sentence[i + 1][1])) + 1) if (
+                    (sentence[i][1], sentence[i + 1][1]) in self.transition_frequency.keys()) else 1
 
     def initialize_probabilities(self):
         """
@@ -74,13 +75,35 @@ class Tagger:
         for key in self.emission_frequency.keys():
             self.emission_probability[key] = float(self.emission_frequency[key] / self.word_frequency[key[0]])
 
-
     def viterbi_decode(self, sentence):
         if type(sentence) != str:
             sys.exit("Incorrect input to method")
-        """
-		YOUR CODE GOES HERE: Complete the rest of the method so that it computes the most likely sequence of tags as described in the question
-		"""
+        probability = 1.0
+        tagging = []
+        words = sentence.split(' ')
+        print("Words of Sentence: ", words)
+        tokens = []
+        for token in self.token_frequency.keys():
+            tokens.append(token)
+        for i in range(0, len(words)):
+            max_probability = []
+            if i == 0:
+                for token in tokens:
+                    if (words[i], token) in self.emission_probability.keys():
+                        max_probability.append(self.initial_tag_probability[token] * self.emission_probability[(words[i], token)])
+                    else:
+                        max_probability.append(self.initial_tag_probability[token] * (1 / self.token_frequency[token]))
+            else:
+                for token in tokens:
+                    if (words[i], token) in self.emission_probability.keys():
+                        max_probability.append(self.transition_probability[(tagging[i-1], token)] * self.emission_probability[(words[i], token)])
+                    else:
+                        max_probability.append(self.transition_probability[(tagging[i-1], token)] * (1 / self.token_frequency[token]))
+
+            index = max_probability.index(max(max_probability))
+            tagging.append(tokens[index])
+
+        return tagging
 
     def get_transition_frequency(self):
         return self.transition_frequency
